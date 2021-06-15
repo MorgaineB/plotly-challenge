4a4f4d18bf8e7e3fcafb2f8693794fd979023c86
Morgaine B <morgainebowers@gmail.com>
2021-06-15 18:27:57 -0400

    Selected relevant html elements

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,9 @@
+//Select all the html elements
+var selector = d3.select("#selDataset");
+var demographics = d3.select("#sample-metadata");
+var barChart = d3.select("#bar");
+var bubbleChart = d3.select("#bubble");
+
 // Use the D3 library to read in samples.json
 d3.json("samples.json").then(function(data) {
     console.log(data);
