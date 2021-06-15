48af9e20a0beb47ff63a4f012331ec02306ca7de
Morgaine B <morgainebowers@gmail.com>
2021-06-15 18:58:31 -0400

    Populated demographics panel with info from json file

--- static/js/app.js
+++ static/js/app.js
@@ -4,6 +4,21 @@ var demographics = d3.select("#sample-metadata");
 var barChart = d3.select("#bar");
 var bubbleChart = d3.select("#bubble");
 
+//Pull info for each sample & add to demographics panel
+function buildDemographics(sample) {
+    d3.json("samples.json").then((data) => {
+        var metadata = data.metadata;
+        var participantMetadata = metadata.filter(sampleobject => sampleobject.id == sample)[0];
+        //console.log(participantMetadata);
+        demographics.html("");
+        Object.entries(participantMetadata).forEach(([key, value]) => {
+            demographics.append("h6").text(`${key}: ${value}`);
+        });
+    });
+}
+
+
+
 //Create an init function to populate the page on load & the drop down menu.
 function init() {
     d3.json("samples.json").then(function(data) {
@@ -20,7 +35,9 @@ function init() {
 
         //get the first sample number in the list and build charts for that sample
         const firstSampleNumber = numbers[0];
-        console.log(firstSampleNumber);
+        // console.log(firstSampleNumber);
+        //pull info & build plots for the first sample using functions from above.
+        buildDemographics(firstSampleNumber);
 
     });
 }
